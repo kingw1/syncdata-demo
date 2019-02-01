8feeca6c3e32bfdc6a97b8542ef807ebb9f818ba
Wichai W <wiwichai@gmail.com>
2019-02-01 17:15:57 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,5 @@
 <?php
 Route::get('/', 'BlogController@index');
 Route::resource('blog', 'BlogController');
+Route::get('/data/push', 'BlogController@pushData');
+Route::post('/data/sync', 'BlogController@syncData');
