ead21c76c0460c8b3674369e01f76215a25c1663
Wichai W <wiwichai@gmail.com>
2019-02-01 13:18:44 +0700

    edit blog index

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,6 +6,7 @@
     <div class="card" style="margin-top: 20px;">
         <div class="card-header bg-primary text-light">{{ $blog->title }}</div>
         <div class="card-body">{!! $blog->content !!}</div>
+        <div class="card-footer">Created At: {{ $blog->created_at }}</div>
     </div>
     @endforeach
 
