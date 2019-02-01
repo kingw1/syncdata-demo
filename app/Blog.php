041e3982ed08f09640367be7abaafcb574379e97
Wichai W <wiwichai@gmail.com>
2019-02-01 17:14:00 +0700

    update fillable blog model

--- app/Blog.php
+++ app/Blog.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    protected $fillable = ['id', 'title', 'content'];
+    protected $fillable = ['id', 'title', 'content', 'is_sync', 'created_at', 'updated_at', 'sync_at'];
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    public $timestamps = false;
 }
