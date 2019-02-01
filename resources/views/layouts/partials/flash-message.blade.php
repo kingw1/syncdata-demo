fc78ceaecd0c0fb07a3217362ebb03562b794f03
Wichai W <wiwichai@gmail.com>
2019-02-01 18:40:55 +0700

    update flash message

--- resources/views/layouts/partials/flash-message.blade.php
+++ resources/views/layouts/partials/flash-message.blade.php
@@ -1,13 +1,11 @@
-@if (Session::has('message-error'))
+@if (session()->has('message-error'))
 <div class="alert alert-danger alert-dismissable">
-	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 	<div>{!! session('message-error') !!}</div>
 </div>
 @endif
 
-@if (Session::has('message-success'))
+@if (session()->has('message-success'))
 <div class="alert alert-success alert-dismissable">
-	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 	<div>{!! session('message-success') !!}</div>
 </div>
 @endif
